081c14c254f8aa775b0feb427ef7514110894bb4
qoudri4re <63318729+qoudri4re@users.noreply.github.com>
2023-01-26 00:26:03 +0100

    Update 1-fifo_cache.py

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -1,27 +1,33 @@
 #!/usr/bin/env python3
-
-from base_caching import BaseCaching
+"""First-In First-Out caching module.
+"""
 from collections import OrderedDict
 
-class FIFOCache(BaseCaching):
-    """represents a cache using the FIFO algo"""
+from base_caching import BaseCaching
 
+
+class FIFOCache(BaseCaching):
+    """Represents an object that allows storing and
+    retrieving items from a dictionary with a FIFO
+    removal mechanism when the limit is reached.
+    """
     def __init__(self):
-        """initializes the cache"""
-        super().__init___()
+        """Initializes the cache.
+        """
+        super().__init__()
         self.cache_data = OrderedDict()
 
     def put(self, key, item):
-        """adds an item to the cache"""
+        """Adds an item in the cache.
+        """
         if key is None or item is None:
             return
         self.cache_data[key] = item
-        if len(self.cache_data) > 4:
-            key, value = self.cache_data.popitem(False)
-            print("DISCARD: ", key)
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            first_key, _ = self.cache_data.popitem(False)
+            print("DISCARD:", first_key)
 
     def get(self, key):
-        """retrieves an item from cached items"""
-        if key is None:
-            return None
+        """Retrieves an item by key.
+        """
         return self.cache_data.get(key, None)
